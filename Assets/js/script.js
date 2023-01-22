58fc8797dcb9e1bbb188dd69674da48a031fb636
scicluna <88948700+scicluna@users.noreply.github.com>
2023-01-22 18:56:58 -0500

    Complete remake of my diceroller

---
+++ Assets/js/script.js
@@ -0,0 +1,129 @@
+//STILL TODO - GET RANDOM.ORG TO WORK INSTEAD OF MATH.RANDOM
+const RANDOM_API_KEY = "3cffdaf3-b80e-42d2-acde-6900ec5086c2"
+
+//DOM SELECT
+const inputString = document.querySelector(".inputstring")
+const inputBtn = document.querySelector(".inputbtn")
+const diceResults = document.querySelector(".results")
+
+inputBtn.addEventListener("click", rollDice)
+
+//run our program
+function rollDice(e){
+e.preventDefault()
+const unsplitDiceArrays = diceRaw(inputString.value) // 3d6, 3d6, 3d6
+const operationArray = operationSplit(inputString.value) // +, -
+
+const splitDiceArrays = diceSplit(unsplitDiceArrays) // [3,6], [3,6]
+const sizeArrays = findSize(splitDiceArrays) // [6, 6]
+
+const rolledDice = diceRoll(splitDiceArrays) // [6,3,1] [6,4,2]
+const addedDice = addDice(rolledDice) // [10,12]
+
+const diceTotal = computeDice(addedDice, operationArray) // 22
+
+writeResults(rolledDice, operationArray, diceTotal, sizeArrays, addedDice)
+}
+
+//find our raw dice inputs
+function diceRaw(input){return input.split(/[-+*\/]/)}
+
+//keep track of our operations for later computation
+function operationSplit(input){return input.split("").filter(character => character.match(/[-+*\/]/))}
+
+//process our raw dice inputs by eliminating the d
+function diceSplit(dicearray){return dicearray.map(dice => dice.split("d"))}
+
+//make an array of dice sizes
+function findSize(dicearray){return dicearray.map(dice=> dice[1])}
+
+//roll our dice
+function diceRoll(dice){return dice.map(die=>rollDie(die))}
+
+//add each dice array together
+function addDice(rolledDice){return rolledDice.map(dice=>dice.reduce((total, item)=>total+item))}
+
+//handles dice rolling
+function rollDie(die){
+    let [quantity, size, drop] = die
+    let explosionFlag = false
+
+    if(size == null) return [parseInt(quantity)]
+    if(size.includes("!")) {
+        size = size.replace("!","")
+        explosionFlag = true
+    }
+
+    const rolled = []
+    for (let i=0; i<quantity; i++){
+        const roll = Math.ceil(Math.random()*size)
+        rolled.push(roll)
+        if(roll == size && explosionFlag == true) i--
+    }
+    
+    rolled.sort((a, b) => b-a)
+    if(drop != null) rolled.splice((rolled.length-drop), drop)
+    
+    return rolled
+}
+
+//adds, subtracts, multiplies, or divides dice
+function computeDice(addedDice, operationArray){
+    let total = addedDice[0]
+    for (let i=1; i<addedDice.length; i++){
+        switch(operationArray[i-1]){
+            case "+": total += addedDice[i]
+            break
+
+            case "-": total -= addedDice[i]
+            break
+
+            case "*": total *= addedDice[i]
+            break
+
+            case "/": total /= addedDice[i]
+            break
+
+            default: break
+        }
+    }
+    return total
+}
+
+//writes our results to the DOM
+function writeResults(rolledDice, operationArray, diceTotal, sizeArrays, addedDice){
+    const newDiv = document.createElement("div")
+
+    rolledDice.forEach((dice, i)=>{
+        if (dice.length > 30) {
+            const newP = document.createElement("i")
+            newP.innerText = addedDice[i]
+            newDiv.appendChild(newP)
+        } else{
+            dice.forEach(die=>{
+                const newP = document.createElement("p")
+                newP.innerText = die
+                const color = findColor(die, i, sizeArrays)
+                if (color) newP.classList.add(color)
+                newDiv.appendChild(newP)
+            
+            })}
+        if(operationArray[i]){
+            const newOp = document.createElement("p")
+            newOp.innerText = operationArray[i]
+            newDiv.appendChild(newOp)
+        }
+    })
+
+    const newBold = document.createElement("b")
+    newBold.innerText = diceTotal
+    newDiv.appendChild(newBold)
+
+    diceResults.appendChild(newDiv)
+}
+
+//color-codes our results
+function findColor(die, i, sizeArrays){
+    if (die == sizeArrays[i].replace("!","")) return "green"
+    if (die == 1) return "red"
+}
